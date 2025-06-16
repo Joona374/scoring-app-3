ab249936744526c37f4ccfc87f6da284a5f2849c
Joona <joona-rahm@hotmail.com>
2025-06-16 21:55:09 +0300

    added some styling and reset App() back to vanilla to start fresh with the frontend development.

--- scoring-frontend/src/App.jsx
+++ scoring-frontend/src/App.jsx
@@ -1,18 +1,4 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("https://scoring-app-3-dev.onrender.com/")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => {
-        console.error(err);
-        setMessage("Failed to fetch");
-      });
-  }, []);
-
-  return <div>{message}</div>;
+  return <div>Hello, world!</div>;
 }
 export default App;
