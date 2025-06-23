fadec7db000b5366fe2eab99234b89cc189884b5
joona374 <Joona.Rahm@student.lab.fi>
2025-06-23 17:52:59 +0300

    Created dummy /me route on backend, and set up dashboard on frontend to fetch data from that

--- scoring-frontend/src/pages/Dashboard/Dashboard.jsx
+++ scoring-frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  const username = "User";
+  const [username, setUsername] = useState("Loading...");
+
+  useEffect(() => {
+    const fetchUserinfo = async () => {
+      const token = localStorage.getItem("jwt_token");
+      const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+      try {
+        const response = await fetch(`${BACKEND_URL}/me`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user info");
+        }
+
+        const data = await response.json();
+        setUsername(data.username);
+      } catch (err) {
+        console.log("Error:", err);
+        setUsername("Error loading username");
+      }
+    };
+
+    fetchUserinfo();
+  }, []);
 
   return (
     <div>
