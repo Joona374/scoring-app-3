cf03cf215d55b9984042c753d941a18cf7cf9e35
joona374 <Joona.Rahm@student.lab.fi>
2025-06-23 17:27:31 +0300

    Implemented navbar

--- scoring-frontend/src/components/navbar/Navbar.jsx
+++ scoring-frontend/src/components/navbar/Navbar.jsx
@@ -1,21 +1,31 @@
 import "./Navbar.css";
+import { useContext } from "react";
+import AuthContext from "../../auth/AuthContext";
 
 export default function Navbar() {
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
     <nav>
       <ul>
         <li>
           <a href="/">Home</a>
         </li>
-        <li>
-          <a href="/register">Register</a>
-        </li>
-        <li>
-          <a href="/login">Login</a>
-        </li>
-        <li>
-          <a href="/dashboard">Dashboard</a>
-        </li>
+        {!isLoggedIn ? (
+          <>
+            {" "}
+            <li>
+              <a href="/register">Register</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </>
+        ) : (
+          <li>
+            <a href="/dashboard">Dashboard</a>
+          </li>
+        )}
       </ul>
     </nav>
   );
