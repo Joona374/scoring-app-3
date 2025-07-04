f7d7d354cdcbb05338571d7e2e4dca80f7bea2e6
joona374 <Joona.Rahm@student.lab.fi>
2025-07-04 14:29:28 +0300

    Refactored excel-test to use openpyxl

--- backend/routes/excel.py
+++ backend/routes/excel.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Response
 import pandas as pd
 from io import BytesIO
+from openpyxl import load_workbook, Workbook
 
 router = APIRouter(
     prefix="/excel",
@@ -10,18 +11,23 @@ router = APIRouter(
 
 @router.get("/download-test")
 async def download_excel():
-    test_data = [
-        {"name": "Joona", "goals": 3},
-        {"name": "Tero", "goals": 69}
-    ]
+    workbook = load_workbook("mock_excel.xlsx")
+    player_template = workbook.active
 
-    df = pd.DataFrame(test_data)
+    names = ["Pekka", "Tero", "Pasi", "Harri", "Kari"]
+
+    for i in range(5):
+        new_sheet = workbook.copy_worksheet(player_template)
+        new_sheet.title = names[i]
+        new_sheet["B1"] = names[i]
+        new_sheet["D1"] = i * 2
+
+    workbook.remove(workbook.worksheets[0])
 
     output = BytesIO()
-    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
-        df.to_excel(writer, index=False, sheet_name="Stats")
-
+    workbook.save(output)
     output.seek(0)
+
     return Response(
         content=output.getvalue(),
         media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
