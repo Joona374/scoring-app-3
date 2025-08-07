72e6a349126a3c792d138196215f4b9db4dddf39
joona374 <Joona.Rahm@student.lab.fi>
2025-08-07 10:29:26 +0300

    Removed accidental duplicate code from merges

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,14 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-
 from db.create_tables import main as wipe_db
-from utils import get_current_user_id, add_creator_code
-from db.db_manager import get_db_session
-from db.models import User, Team
-from db.pydantic_schemas import CreateCode, CreateCodeResponse
 from utils import get_current_user_id, add_creator_code
 from db.db_manager import get_db_session
 from db.models import User, Team
@@ -30,15 +23,6 @@ router = APIRouter(
 #         return {"Message": "DB WIPING FAILED", "ERROR": e}
 ###### ONLY FOR EMERGENCY USE! ########
 
-###### ONLY FOR EMERGENCY USE! ########
-# @router.post("/clean-db")
-# def clean_db():
-#     try:
-#         creator_code = wipe_db()
-#         return {"Message": "Wiped db!", "creator_code": creator_code}
-#     except Exception as e:
-#         return {"Message": "DB WIPING FAILED", "ERROR": e}
-###### ONLY FOR EMERGENCY USE! ########
 
 @router.post("/clean-db")
 def clean_db(db_session: Session = Depends(get_db_session), current_user_id: int = Depends(get_current_user_id)):
